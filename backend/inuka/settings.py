541295f975e38cd021b75220c1a92043f7bf28b6
Nelson Gabriel <pressagioone@gmail.com>
2026-08-19 23:06:45 +0200

    Fix: Corrigir backend para produção

--- backend/inuka/settings.py
+++ backend/inuka/settings.py
@@ -33,6 +33,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'inuka.urls'
 
