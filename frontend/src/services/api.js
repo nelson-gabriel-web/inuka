c4d14f72d91c863524fd6892b614a3fbc826aa6d
Nelson Gabriel <pressagioone@gmail.com>
2026-08-19 03:57:41 +0200

    Fix: Frontend com .env e build

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://inuka-6576.onrender.com/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
